a73e4a5851959769b0ec55d7c9f1109c828cdfe7
meg <101708422+margheritabagnolini@users.noreply.github.com>
2024-11-01 15:29:36 +0100

    step two provisory components

---
+++ magicShelf/src/components/registration/StepTwoCostumer.vue
@@ -0,0 +1,25 @@
+<script setup>
+// import PageTitleNavigation from '@/components/PageTitleNavigation.vue'
+// import { useRegistrationStore } from '@/stores/registration'
+// import { storeToRefs } from 'pinia';
+// import { computed } from 'vue';
+// import { useConfigurationStore } from '@/stores/configurations'
+// import { UserType } from '@/models/user-type'
+
+// const registrationStore = useRegistrationStore();
+// const { registration, getDropdownString } = storeToRefs(registrationStore);
+
+// const dropDownString = computed(() => getDropdownString.value);
+
+</script>
+
+<template>
+    <main>
+        <div class="mx-4">
+            <p>costumer</p>
+        </div>
+    </main>
+</template>
+  
+  
+<style scoped></style>
