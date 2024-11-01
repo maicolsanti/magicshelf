7f3820b9ac79c30d541be65670d4c407885655f8
meg <101708422+margheritabagnolini@users.noreply.github.com>
2024-11-01 15:27:05 +0100

    page title navigation component

---
+++ magicShelf/src/components/PageTitleNavigation.vue
@@ -0,0 +1,50 @@
+<script setup>
+import '@/assets/scss/custom-css.css'
+import { addIcons } from 'oh-vue-icons'
+import { IoArrowBackCircleOutline, IoArrowBackCircle } from "oh-vue-icons/icons";
+import { useRouter } from 'vue-router'
+defineProps({
+    subTitle: {
+        type: String,
+        required: false
+    },
+    goPreviousStep: {
+        type: Boolean,
+        required: false
+    }
+})
+
+addIcons(IoArrowBackCircleOutline, IoArrowBackCircle);
+
+const router = useRouter()
+const goBack = () => {
+    router.go(-1)
+}
+
+</script>
+<template>
+    <div class="row align-items-end">
+        <div class="col d-flex flex-column align-items-start">
+            <button @click="goBack"><v-icon name="io-arrow-back-circle" scale="2.3" class="navigation-icon" /></button>
+        </div>
+        <div class="col sub-page-title d-flex flex-column align-items-end">
+            <h4>{{ subTitle }}</h4>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.navigation-icon {
+    color: var(--bs-primary);
+}
+
+.sub-page-title {
+    align-items: end;
+    align-content: end;
+}
+
+button {
+    background-color: unset;
+    border: unset;
+}
+</style>
