65ebe8c873c587d6481fa7fc1102583278214e31
meg <101708422+margheritabagnolini@users.noreply.github.com>
2024-11-01 10:54:23 +0100

    app navbar component

---
+++ magicShelf/src/components/AppNavbar.vue
@@ -0,0 +1,66 @@
+<script setup>
+import '@/assets/scss/custom-css.css'
+import { page } from '@/models/page'
+import { useConfigurationStore } from '@/stores/configurations'
+import { storeToRefs } from 'pinia';
+
+const confStore = useConfigurationStore();
+const { updatePageName } = storeToRefs(confStore);
+
+</script>
+<template>
+  <nav class="navbar navbar-expand-lg bg-primary shadow-sm mb-5">
+    <div class="container-fluid">
+      <a class="navbar-brand page-name" href="#">{{ updatePageName }}</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item mx-2" @click="confStore.updatePage(page.HOME)">
+            <RouterLink to="/" class="nav-link inactive-page" active-class="active-page">Home</RouterLink>
+          </li>
+          <li class="nav-item mx-2" @click="confStore.updatePage(page.SEARCH)">
+            <RouterLink to="/search" class="nav-link inactive-page" active-class="active-page">Ricerca</RouterLink>
+          </li>
+          <li class="nav-item mx-2" @click="confStore.updatePage(page.PRODUCTS)">
+            <RouterLink to="/products" class="nav-link inactive-page" active-class="active-page">Prodotti</RouterLink>
+          </li>
+          <li class="nav-item mx-2" @click="confStore.updatePage(page.PROFILE)">
+            <RouterLink to="/profile" class="nav-link inactive-page" active-class="active-page">Profilo</RouterLink>
+          </li>
+          <li class="nav-item mx-2" @click="confStore.updatePage(page.ABOUT)">
+            <RouterLink to="/about" class="nav-link inactive-page" active-class="active-page">About</RouterLink>
+          </li>
+          <!-- TODO replace with icon -->
+          <li class="nav-item mx-2" @click="confStore.updatePage(page.LOGIN)">
+            <RouterLink to="/login" class="nav-link inactive-page" active-class="active-page">Accedi</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.page-name {
+  color: white;
+}
+
+.inactive-page {
+  font-family: 'Rubik', Rubik, sans-serif;
+  font-weight: lighter;
+  color: white;
+}
+
+.active-page {
+  font-family: 'Rubik', Rubik, sans-serif;
+  font-weight: bold;
+  color: white;
+  text-shadow: -1px 1px 0 var(--bs-secondary),
+    1px 1px 0 var(--bs-secondary),
+    1px -1px 0 var(--bs-secondary),
+    -1px -1px 0 var(--bs-secondary);
+}
+</style>
