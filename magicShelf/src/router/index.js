a665e47cf6ffddad47a3d1e2256346c46f8cc853
meg <101708422+margheritabagnolini@users.noreply.github.com>
2024-11-01 10:53:17 +0100

    added main pages to router

--- magicShelf/src/router/index.js
+++ magicShelf/src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SearchView from '../views/SearchView.vue'
+import ProductsView from '../views/ProductsView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import AboutView from '../views/AboutView.vue'
+import LoginView from '../views/login/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +15,29 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/search',
+      name: 'search',
+      component: SearchView
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: ProductsView
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView
+    },
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
     }
   ]
 })
