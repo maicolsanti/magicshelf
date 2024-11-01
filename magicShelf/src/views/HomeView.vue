edee7575623910f41b31db420b98ba3f325ca707
meg <101708422+margheritabagnolini@users.noreply.github.com>
2024-11-01 11:45:44 +0100

    home view

--- magicShelf/src/views/HomeView.vue
+++ magicShelf/src/views/HomeView.vue
@@ -1,3 +1,28 @@
+<script setup>
+import SearchInstructions from '@/components/home/SearchInstructions.vue'
+import SupplierLink from '@/components/home/SupplierLink.vue'
+import { useConfigurationStore } from '@/stores/configurations'
+import { storeToRefs } from 'pinia';
+
+const confStore = useConfigurationStore();
+const { configurations } = storeToRefs(confStore);
+</script>
+
 <template>
-  <p>Home page</p>
+  <main>
+    <h1 class="welcome mb-4">Benvenut*,<br>qui potrai trovare ciò che cerchi
+    </h1>
+    <div class="d-grid gap-2 mb-1" v-if="!configurations.isLoggedIn">
+      <button type="button" class="btn btn-primary py-2 login-button">Accedi per iniziare la ricerca</button>
+    </div>
+    <SupplierLink v-if="!configurations.isLoggedIn" />
+    <SearchInstructions v-if="!configurations.isLoggedIn" />
+  </main>
 </template>
+
+
+<style scoped>
+.login-button {
+  max-width: 350px;
+}
+</style>
