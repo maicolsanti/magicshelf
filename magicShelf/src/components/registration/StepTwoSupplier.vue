344163584a0caf924d799cbeb0ed5425c47cb091
Margherita Bagnolini <margheritabagnolini01@gmail.com>
2024-11-21 12:18:17 +0100

    updated grid in supplier registration

--- magicShelf/src/components/registration/StepTwoSupplier.vue
+++ magicShelf/src/components/registration/StepTwoSupplier.vue
@@ -82,54 +82,56 @@ function submit() {
             <div class="container">
                 <h5 class="form-section-title">Anagrafici e di contatto</h5>
                 <div class="row form-group">
-                    <div class="form-group mb-3">
+                    <div class="col-4 mb-3 form-column">
                         <label for="">Nome</label>
                         <input type="text" class="form-control" id="nameInput" v-model="formData.nameInput"
                             placeholder="nome" required />
                     </div>
-                    <div class="form-group mb-3">
+                    <div class="col-4 mb-3 form-column">
                         <label for="">Cognome</label>
                         <input type="text" class="form-control" id="surnameInput" v-model="formData.surnameInput"
                             placeholder="cognome" required />
                     </div>
-                </div>
-                <div class="row form-group">
-                    <div class="form-group mb-3">
+                    <div class="col-4 mb-3 form-column">
                         <label for="">Ragione Sociale</label>
                         <input type="text" class="form-control" id="companyNameInput" v-model="formData.companyNameInput"
                             placeholder="ragione sociale" required />
                     </div>
                 </div>
                 <div class="row form-group">
-                    <div class="form-group mb-1">
+                    <div class="col-4 mb-2 form-column">
                         <label for="">Partita Iva</label>
                         <input type="text" class="form-control" id="vatNumberInput" v-model="formData.vatNumberInput"
                             placeholder="p. iva" required />
+                        <div class="form-check mb-1 d-flex align-items-center">
+                            <input type="checkbox" id="checkbox" v-model="isVatSame" class="form-check-input vat-checkbox"
+                                @click="setFiscalCodeWithVatNumber" />
+                            <label for="checkbox" class="form-check-label vat-checkbox-label ms-2">La partita iva
+                                corrisponde al
+                                codice fiscale</label>
+                        </div>
                     </div>
-                    <div class="form-check mb-3 d-flex align-items-center">
-                        <input type="checkbox" id="checkbox" v-model="isVatSame" class="form-check-input vat-checkbox"
-                            @click="setFiscalCodeWithVatNumber" />
-                        <label for="checkbox" class="form-check-label vat-checkbox-label ms-2">La partita iva corrisponde al
-                            codice fiscale</label>
+                    <div class="col-4 mb-3 form-column">
+                        <div class="form-group mb-3" v-if="!isVatEquivalent">
+                            <label for="">Codice Fiscale</label>
+                            <input type="text" class="form-control" id="fiscalCodeInput" v-model="formData.fiscalCodeInput"
+                                placeholder="codice fiscale" required />
+                        </div>
+                        <div class="form-group mb-3 disabled" v-if="isVatEquivalent">
+                            <label for="">Codice Fiscale</label>
+                            <input type="text" class="form-control disabled" :disabled="true" id="fiscalCodeInputSame"
+                                v-model="formData.vatNumberInput" placeholder="codice fiscale" required />
+                        </div>
                     </div>
-                    <div class="form-group mb-3" v-if="!isVatEquivalent">
-                        <label for="">Codice Fiscale</label>
-                        <input type="text" class="form-control" id="fiscalCodeInput" v-model="formData.fiscalCodeInput"
-                            placeholder="codice fiscale" required />
-                    </div>
-                    <div class="form-group mb-3 disabled" v-if="isVatEquivalent">
-                        <label for="">Codice Fiscale</label>
-                        <input type="text" class="form-control disabled" :disabled="true" id="fiscalCodeInputSame"
-                            v-model="formData.vatNumberInput" placeholder="codice fiscale" required />
-                    </div>
+
                 </div>
                 <div class="row form-group">
-                    <div class="form-group mb-3">
+                    <div class="col-4 mb-3 form-column">
                         <label for="">Email</label>
                         <input type="email" class="form-control" id="emailInput" v-model="formData.emailInput"
                             placeholder="indirizzo email" required />
                     </div>
-                    <div class="form-group mb-3">
+                    <div class="col-4 mb-3 form-column">
                         <label for="">Telefono</label>
                         <input type="tel" class="form-control" id="phoneInput" v-model="formData.phoneInput"
                             placeholder="numero di telefono" required />
@@ -137,28 +139,34 @@ function submit() {
                 </div>
                 <h5 class="form-section-title mt-2">Località</h5>
                 <div class="row form-group">
-                    <div class="form-group mb-3">
+                    <div class="col-4 mb-3 form-column">
                         <label for="">CAP</label>
                         <input type="text" class="form-control" id="CAPInput" v-model="formData.CAPInput" placeholder="cap"
                             required @change="registrationStore.changeCAP(formData.CAPInput)" />
                     </div>
-                    <div class="dropdown d-flex justify-content-center mb-3">
-                        <button class="btn dropdown-toggle cap-dropdown d-flex justify-content-between align-items-center"
-                            type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="true">
-                            <span class="dropdown-text" :class="{ 'town-not-selected': townData == '' }">{{
-                                dropDownString }}</span>
-                            <span class="dropdown-icon"></span>
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" role="menu">
-                            <li>
-                                <a v-if="towns.length == 0" class="dropdown-item disabled"
-                                    @click="registrationStore.setSelectedTown(town.name)" href="#">Inserisci il cap</a>
-                                <a v-if="towns.length > 0" v-for="   town    in    towns   " v-bind:key="town.name"
-                                    class="dropdown-item" @click="registrationStore.setSelectedTown(town.name)" href="#">{{
-                                        town.name }}</a>
-                            </li>
-                        </ul>
+                    <div class="col-4 mb-3 form-column">
+                        <label for="">Comune</label>
+                        <div class="dropdown d-flex justify-content-center mb-3">
+                            <button
+                                class="btn dropdown-toggle cap-dropdown d-flex justify-content-between align-items-center"
+                                type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="true">
+                                <span class="dropdown-text" :class="{ 'town-not-selected': townData == '' }">{{
+                                    dropDownString }}</span>
+                                <span class="dropdown-icon"></span>
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" role="menu">
+                                <li>
+                                    <a v-if="towns.length == 0" class="dropdown-item disabled"
+                                        @click="registrationStore.setSelectedTown(town.name)" href="#">Inserisci il cap</a>
+                                    <a v-if="towns.length > 0" v-for="   town    in    towns   " v-bind:key="town.name"
+                                        class="dropdown-item" @click="registrationStore.setSelectedTown(town.name)"
+                                        href="#">{{
+                                            town.name }}</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
+
                 </div>
                 <h5 class="form-section-title mt-1">Account</h5>
                 <div class="row form-group">
@@ -176,92 +184,6 @@ function submit() {
                         </div>
                     </div>
                 </div>
-
-
-                <!-- <div class="form-g mb-5">
-                    <div class="form-group mb-3">
-                        <label for="">Nome</label>
-                        <input type="text" class="form-control" id="nameInput" v-model="formData.nameInput"
-                            placeholder="nome" required />
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="">Cognome</label>
-                        <input type="text" class="form-control" id="surnameInput" v-model="formData.surnameInput"
-                            placeholder="cognome" required />
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="">Ragione Sociale</label>
-                        <input type="text" class="form-control" id="companyNameInput" v-model="formData.companyNameInput"
-                            placeholder="ragione sociale" required />
-                    </div>
-                    <div class="form-group mb-1">
-                        <label for="">Partita Iva</label>
-                        <input type="text" class="form-control" id="vatNumberInput" v-model="formData.vatNumberInput"
-                            placeholder="p. iva" required />
-                    </div>
-                    <div class="form-check mb-3 d-flex align-items-center">
-                        <input type="checkbox" id="checkbox" v-model="isVatSame" class="form-check-input vat-checkbox"
-                            @click="setFiscalCodeWithVatNumber" />
-                        <label for="checkbox" class="form-check-label vat-checkbox-label ms-2">La partita iva corrisponde al
-                            codice fiscale</label>
-                    </div>
-                    <div class="form-group mb-3" v-if="!isVatEquivalent">
-                        <label for="">Codice Fiscale</label>
-                        <input type="text" class="form-control" id="fiscalCodeInput" v-model="formData.fiscalCodeInput"
-                            placeholder="codice fiscale" required />
-                    </div>
-                    <div class="form-group mb-3 disabled" v-if="isVatEquivalent">
-                        <label for="">Codice Fiscale</label>
-                        <input type="text" class="form-control disabled" :disabled="true" id="fiscalCodeInputSame"
-                            v-model="formData.vatNumberInput" placeholder="codice fiscale" required />
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="">Email</label>
-                        <input type="email" class="form-control" id="emailInput" v-model="formData.emailInput"
-                            placeholder="indirizzo email" required />
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="">Telefono</label>
-                        <input type="tel" class="form-control" id="phoneInput" v-model="formData.phoneInput"
-                            placeholder="numero di telefono" required />
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="">CAP</label>
-                        <input type="text" class="form-control" id="CAPInput" v-model="formData.CAPInput" placeholder="cap"
-                            required @change="registrationStore.changeCAP(formData.CAPInput)" />
-                    </div>
-                    <div class="dropdown d-flex justify-content-center mb-3">
-                        <button class="btn dropdown-toggle cap-dropdown d-flex justify-content-between align-items-center"
-                            type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="true">
-                            <span class="dropdown-text" :class="{ 'town-not-selected': townData == '' }">{{
-                                dropDownString }}</span>
-                            <span class="dropdown-icon"></span>
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" role="menu">
-                            <li>
-                                <a v-if="towns.length == 0" class="dropdown-item disabled"
-                                    @click="registrationStore.setSelectedTown(town.name)" href="#">Inserisci il cap</a>
-                                <a v-if="towns.length > 0" v-for="   town    in    towns   " v-bind:key="town.name"
-                                    class="dropdown-item" @click="registrationStore.setSelectedTown(town.name)" href="#">{{
-                                        town.name }}</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="form-group row">
-                        <label for="">Password</label>
-                        <div class="col-md-13 input-group">
-                            <input :type="passwordInputType" class="form-control mb-5" id="passwordInput"
-                                v-model="formData.passwordInput" placeholder="password" required />
-                            <div class="input-group-btn">
-                                <button class="btn icon-button" @click="registrationStore.toggleShowPassword()">
-                                    <v-icon v-if="!registration.showPassword" name="md-visibility-round"
-                                        class="visibility-icon" />
-                                    <v-icon v-if="registration.showPassword" name="md-visibilityoff-round"
-                                        class="visibility-icon" />
-                                </button>
-                            </div>
-                        </div>
-                    </div> -->
                 <div class="d-flex justify-content-center">
                     <button @click="submit" class="btn btn-primary form-button mb-2">
                         Registrati
@@ -336,18 +258,15 @@ label {
 }
 
 .dropdown-select {
-    width: 350px;
+    max-width: 350px;
     height: 36px;
     border-color: var(--bs-secondary);
     border-radius: 6px;
     padding-left: 10px;
 }
 
-.dropdown-menu {
-    width: 350px;
-}
-
 .cap-dropdown {
+    max-width: 350px;
     width: 350px;
     align-content: start;
     align-items: start;
@@ -358,6 +277,10 @@ label {
 .town-not-selected {
     color: var(--bs-secondary);
     opacity: 0.5;
+}
+
+.input-group {
+    max-width: 350px;
 }
 
 .alert {
