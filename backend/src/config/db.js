e766c4eaff1e01a2f1226e64bdaa6d70ad23a6d2
MaicolSanti <smaicol02@gmail.com>
2024-12-30 15:39:29 +0100

    ref: deleted unused comments

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,15 +6,10 @@ dotenv.config();
 
 // Configurazione della connessione al database
 const pool = mysql.createPool({
-  // eslint-disable-next-line no-undef
   host: process.env.DB_HOST, // Host
-  // eslint-disable-next-line no-undef
   user: process.env.DB_USER, // User
-  // eslint-disable-next-line no-undef
   password: process.env.DB_PASSWORD, // Pwd
-  // eslint-disable-next-line no-undef
   database: process.env.DB_NAME, // DBname
-  // eslint-disable-next-line no-undef
   port: process.env.DB_PORT || 3306 // DBport
 });
 
