05f83843b68b9e2d82e386663e72f3805c1658d6
meg <101708422+margheritabagnolini@users.noreply.github.com>
2024-12-31 17:27:30 +0100

    updated mocked supplier in fetched produccts

--- magicShelf/src/stores/product.ts
+++ magicShelf/src/stores/product.ts
@@ -19,7 +19,7 @@ export const useProductsStore = defineStore('productStore', {
   actions: {
     async fetchSearchedProducts() {
       var fetchedProducts = [
-        new Product(1, "Penna", "Bic", "pezzi", 2.5, new Supplier(1, "n", "s", "Bic", "11111111111", "11111111111", "011/111111", " bic@bic.com", "via rossi, 1", 10100, 1001)),
+        new Product(1, "Penna", "Bic", "pezzi", 2.5, new Supplier(1, "Maicol", "Santi", "Bic", "11111111111", "11111111111", "011/111111", " bic@bic.com", "Via rossi, 1", 10100, 1001)),
         new Product(2, "Matita", "Faber", "pezzi", 1.5, new Supplier(2, "n", "s", "Faber", "22222222222", "22222222222", "011/222222", "faber@faber.com", "via rossi, 2", 47020, 1002)),
         new Product(3, "Gomma", "Staedtler", "pezzi", 3, new Supplier(3, "n", "s", "Staedtler", "33333333333", "33333333333", "011/333333", "staedtler@staedtler.com", "via rossi, 3", 10100, 1003)),
       ]
