3b6d1ce0eb5ac56ee4762f32f0e1d63e5a0f1107
meg <101708422+margheritabagnolini@users.noreply.github.com>
2025-01-25 10:16:49 +0100

    updated getFiltered

--- magicShelf/src/stores/product.ts
+++ magicShelf/src/stores/product.ts
@@ -21,15 +21,6 @@ export const useProductsStore = defineStore('productStore', {
     },
   }),
   actions: {
-    async fetchSearchedProducts() {
-      var fetchedProducts = [
-        new Product(1, "Penna", "Bic", "pezzi", 2.5, new Supplier(1, "Maicol", "Santi", "Bic", "11111111111", "11111111111", "011/111111", " bic@bic.com", "Via rossi, 1", 10100, 1001)),
-        new Product(2, "Matita", "Faber", "pezzi", 1.5, new Supplier(2, "n", "s", "Faber", "22222222222", "22222222222", "011/222222", "faber@faber.com", "via rossi, 2", 47020, 1002)),
-        new Product(3, "Gomma", "Staedtler", "pezzi", 3, new Supplier(3, "n", "s", "Staedtler", "33333333333", "33333333333", "011/333333", "staedtler@staedtler.com", "via rossi, 3", 10100, 1003)),
-      ]
-      this.products = fetchedProducts;
-      return true;
-    },
     async getFilteredProducts() {
       var priceRange = [];
       switch (this.filters.priceRange) {
@@ -49,15 +40,20 @@ export const useProductsStore = defineStore('productStore', {
           priceRange = [0, 100];
       }
       const distanceRange = this.filters.distanceRange == DistanceRange.SAMEISTATCODE ? "Dentro al comune" : "Fuori dal comune";
-      console.log("produt name: " + this.filters.productName);
-      const requestBodyFilters = {
+      const filters = {
         "DESCRIZIONE_MATERIALE": this.filters.productName,
         "MARCA": this.filters.brand,
         "FORNITORE": this.filters.supplier,
         "PREZZO_UNITARIO": priceRange,
-        "ZONA_DI_RICERCA": distanceRange, //TODO chiedi a maic se come default va bene
-        "ZONA_DI_PARTENZA": "Longiano" //TODO CAMBIARE E IMPOSTARE ZONA GIUSTA
+        "ZONA_DI_RICERCA": distanceRange,
+        "ZONA_DI_PARTENZA": this.filters.cap
       };
+      const requestBodyFilters = Object.entries(filters).reduce((acc, [key, value]) => {
+        if (value !== "" && value !== null) {
+          acc[key] = value;
+        }
+        return acc;
+      }, {} as Record<string, any>);
       console.log("filters:" + JSON.stringify(requestBodyFilters));
       try {
         this.products = await axios.post('/api/materiali-fornitori/getFiltered', requestBodyFilters).then(res => res.data.map((product: any) => new FoundProduct(
