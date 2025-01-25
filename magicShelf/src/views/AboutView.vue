9af20e40d10daaa13d46938857eff4155c8f9706
meg <101708422+margheritabagnolini@users.noreply.github.com>
2025-01-25 16:23:13 +0100

    updated About Us page with description

--- magicShelf/src/views/AboutView.vue
+++ magicShelf/src/views/AboutView.vue
@@ -1,3 +1,12 @@
 <template>
-  <p>About us page</p>
+  <h2 class="welcome">Siamo Margherita e Maicol, due studenti dell'UNIBO.
+    Abbiamo creato questo sito per il progetto d'esame di Ingegneria e sistemi web.
+  <br>
+  <br>
+  Il progetto si propone di realizzare un'applicazione web moderna e innovativa, 
+  progettata per rispondere alle esigenze di un mercato sempre più dinamico e 
+  interconnesso. L'app ha l'obiettivo di creare un punto di incontro tra acquirenti 
+  e fornitori, offrendo loro strumenti avanzati per semplificare la ricerca e 
+  la gestione dei prodotti.
+  </h2>
 </template>
