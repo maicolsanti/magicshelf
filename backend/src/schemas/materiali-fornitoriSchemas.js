438f15d9693025a1971327024e7c546ee8294bce
MaicolSanti <smaicol02@gmail.com>
2025-01-29 21:42:06 +0100

    fix: changed getFilteredSchema validation schema

--- backend/src/schemas/materiali-fornitoriSchemas.js
+++ backend/src/schemas/materiali-fornitoriSchemas.js
@@ -1,10 +1,10 @@
 import Joi from 'joi';
 
 export const getFilteredSchema = Joi.object({
-  DESCRIZIONE_MATERIALE: Joi.string().required(),
-  MARCA: Joi.string().required(),
-  FORNITORE: Joi.string().required(),
-  PREZZO_UNITARIO: Joi.array().items(Joi.number().required()).length(2).required(),
+  DESCRIZIONE_MATERIALE: Joi.string().optional(),
+  MARCA: Joi.string().optional(),
+  FORNITORE: Joi.string().optional(),
+  PREZZO_UNITARIO: Joi.array().items(Joi.number().required()).length(2).optional(),
   ZONA_DI_RICERCA: Joi.string().valid('Fuori dal Comune', 'Dentro al Comune').required(),
   ZONA_DI_PARTENZA: Joi.string().required(),
 });
